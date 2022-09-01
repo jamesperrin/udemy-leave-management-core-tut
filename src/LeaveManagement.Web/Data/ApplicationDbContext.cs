4a01ca9588fcaff86e358465e6f6ba438e873c80
James Perrin <countrydawgg@gmail.com>
2022-08-31 18:47:38 -0700

    Add Employee.cs and migration ExtendedUserTable

--- src/LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ src/LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-		}
-	}
+    public class ApplicationDbContext : IdentityDbContext<Employee>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
 }
---
+++ src/LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        [Column(TypeName = "varchar(256)")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "varchar(256)")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "varchar(256)")]
+        public string TaxId { get; set; }
+
+        [Column(TypeName = "datetime2(4)")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Column(TypeName = "datetime2(4)")]
+        public DateTime DateJoined { get; set; }
+    }
+}
