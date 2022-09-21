0d06c6d3420989e5d2cd2e896fa95b1c83e05c8c
James Perrin <countrydawgg@gmail.com>
2022-09-21 16:18:49 -0700

    Complete Section 7 - User Authentication and Authorization with IdentityCore

--- src/LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ src/LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -1,16 +1,25 @@
+using LeaveManagement.Web.Configurations.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-	public class ApplicationDbContext : IdentityDbContext<Employee>
-	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-			: base(options)
-		{
-		}
+    public class ApplicationDbContext : IdentityDbContext<Employee>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-		public DbSet<LeaveType> LeaveTypes { get; set; }
-		public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
-	}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleSeedConfiguration());
+            builder.ApplyConfiguration(new UserSeedConfiguration());
+            builder.ApplyConfiguration(new UserRoleSeedConfiguration());
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
 }
